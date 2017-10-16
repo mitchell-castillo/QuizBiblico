57b2a9f88149f3ff294001752a5f2c2310a5d6bf
Mitchell <juanluis.mitchell@gmail.com>
2017-10-16 15:26:02 -0400

    Update QuizBiblico.js

--- QuizBiblico.js
+++ QuizBiblico.js
@@ -1,4 +1,15 @@
+"Preguntas" :
+[
 {
-	"nombre" : "Juan",
-	"apellido" : "Mitchell"
+	"Descripcion" : "Quien es Jesus?",
+	"citaBiblica" : "Juan 3:16",
+	"Libro" : "Genesis",
+	"Respuestas" : 
+	[
+		{ "Descripcion" : "El hijo de Dios", "esCorrecta" : true },
+		{ "Descripcion" : "El hijo de abraham", "esCorrecta" : false },
+		{ "Descripcion" : "El hijo de jeremias", "esCorrecta" : false },
+		{ "Descripcion" : "El hijo de adam", "esCorrecta" : false }
+	]
 }
+]
